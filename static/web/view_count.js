028f9c0435e0e09a8be02cfdc28d6eb4e56ec73d
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-02-04 22:51:51 +0800

    尝试添加自动添加空格脚本

--- static/web/view_count.js
+++ static/web/view_count.js
@@ -4,7 +4,7 @@ function createVercount() {
         postBody.insertAdjacentHTML('afterend', '<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文浏览量<span id="busuanzi_value_page_pv"></span>次</div>');
     }
     var runday = document.getElementById('runday');
-    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">总浏览量<span id="busuanzi_value_site_pv"></span>次 • </span>');
+    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">总浏览量 <span id="busuanzi_value_site_pv"></span> 次 • </span>');
 }
 document.addEventListener("DOMContentLoaded", function () {
     createVercount();
