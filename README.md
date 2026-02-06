918c2df824d2f253e0c9c15d483b80a9356c9900
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-02-06 13:17:18 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [22](https://MaxLHy0424.is-a.dev/tag.html) 
 ### :speech_balloon: 3 
 ### :hibiscus: 59521 
-### :alarm_clock: 2026-02-06 21:10:42 
+### :alarm_clock: 2026-02-06 21:17:18 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/web/view_count.js
+++ docs/web/view_count.js
@@ -1,7 +1,7 @@
 function createVercount() {
     var postBody = document.getElementById('postBody');
     if (postBody) {
-        postBody.insertAdjacentHTML('afterend', '<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文浏览量<span id="busuanzi_value_page_pv"></span>次</div>');
+        postBody.insertAdjacentHTML('afterend', '<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文浏览量 <span id="busuanzi_value_page_pv"></span> 次</div>');
     }
     var runday = document.getElementById('runday');
     runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">总浏览量 <span id="busuanzi_value_site_pv"></span> 次 • </span>');
@@ -11,5 +11,4 @@ document.addEventListener("DOMContentLoaded", function () {
     var element = document.createElement('script');
     element.src = 'https://vercount.one/js';
     document.head.appendChild(element);
-    console.log("\n %c GmeekVercount Plugins https://github.com/Meekdai/Gmeek \n", "padding:5px 0;background:#bc4c00;color:#fff");
 });
